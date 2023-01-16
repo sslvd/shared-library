3559577945fa95f385be8937c3776b60eeaf8653
sslvd <122521693+sslvd@users.noreply.github.com>
2023-01-16 11:13:00 +0000

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(){
-  input message:'terraform destroy?',ok:'yes',
-                 sh("terraform -auto-approve")
+  input message:'terraform destroy?',ok:'yes'
+                 sh("terraform -auto-approve");
                  }
